6da94a5bb4973367b8924281de4be05c55a613bd
Daniel Kray <dankray@gmail.com>
2025-04-18 22:11:28 -0500

    HOTSdle-ify

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import * as db from './db';
-import type { SelectScoreWithRelations } from './db/schema';
+import { Client, Events, GatewayIntentBits, TextChannel } from 'discord.js';
 
 const client = new Client({
   intents: [
@@ -9,122 +7,51 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-const wordlePattern = /Wordle (\d{0,3}(,?)\d{1,3}) (🎉 ?)?([X1-6])\/6/;
-type WordleResult = {
-  discordId: string;
-  userName: string;
-  gameNumber: number;
-  attempts: string;
-};
-let wordleResults: WordleResult[] = [];
+
+const cron = require('cron');
+
+const currentDate = new Date();
+
 
 client.once(Events.ClientReady, async (readyClient) => {
-  console.log(`Logged in as ${readyClient.user?.tag}`);
+    console.log(`Logged in as ${readyClient.user?.tag}`);
+
+     //'00 30 10 * * *' 10:30
+    let scheduledMessage = new cron.CronJob('00 25 22 * * *', () => {
+    // This runs every day at 10:30:00, you can do anything you want
+    const guild = readyClient.guilds.cache.first();
+    const channel = guild?.channels.cache.find(channel => channel.name == 'stinger_gang');
+    if (!channel) {
+        console.log("No channel found!");
+        return;
+    }
+
+    const messageChannel = readyClient.channels.cache.get(channel.id) as TextChannel;
+    if (!messageChannel) {
+        console.log("messageChannel undefined");
+        return;
+    }
+    messageChannel.send('@here Daily HOTSdle!\nhttps://hotsdle.zgame.studio/hero-guesser');
+    });
+          
+    // When you want to start it, use:
+    scheduledMessage.start()
 });
 
 client.on(Events.MessageCreate, async (message) => {
-  const parsedWordle = parseWordleResult(message);
-  if (parsedWordle) {
-    const currentResults = await processLatestWordleResult(parsedWordle);
-    await processCurrentResults(currentResults, message);
-  } else {
-    console.log('Message was determined to not be intended for the bot');
+  console.log(`${message.author.tag} said ${message.content}`);
+
+  if (!client.user) return
+  if(message.author.id === client.user.id) return
+  if (message.content.toLowerCase().includes('hotsdle')) {
+    sendHotsdleMessage(message);
   }
 });
 
+async function sendHotsdleMessage(message: any) {
+    message.channel.send(`
+        <@${message.author.id}> asuh dud:\nhttps://hotsdle.zgame.studio/hero-guesser`);
+}
+
+
 await client.login(process.env.DISCORD_BOT_TOKEN);
-
-function parseWordleResult(message: any): WordleResult | undefined {
-  const userName = message.author.username;
-  const discordId = message.author.id;
-  const match = wordlePattern.exec(message.content);
-
-  if (match) {
-    const gameNumber = parseInt(match[1].replace(/,/g, ''));
-    const attempts = match[4];
-
-    return {
-      discordId,
-      userName,
-      gameNumber,
-      attempts,
-    };
-  }
-
-  return undefined;
-}
-
-async function processLatestWordleResult(parsedWordle: WordleResult): Promise<SelectScoreWithRelations[]> {
-  // Prevent duplicates
-  const scoresForCurrentGame = await db.getScoresByGameNumber(parsedWordle.gameNumber);
-  const existingResultForUser = scoresForCurrentGame.find((score: SelectScoreWithRelations) => score.discordId === parsedWordle.discordId);
-  if (!existingResultForUser) {
-    await db.createPlayer(parsedWordle.discordId, parsedWordle.userName);
-    if(scoresForCurrentGame.length === 0) {
-      await db.createWordle(parsedWordle.gameNumber);
-    }
-    const addedScore = await db.createScore(parsedWordle.discordId, parsedWordle.gameNumber, parsedWordle.attempts);
-    if(addedScore){
-      scoresForCurrentGame.push(addedScore);
-    } else {
-      console.error(`Error adding result to the database: ${parsedWordle.gameNumber} - ${parsedWordle.userName}`);
-    }
-  } else {
-    console.log(`Result already exists: ${parsedWordle.gameNumber} - ${parsedWordle.userName}`);
-  }
-  return scoresForCurrentGame;
-}
-
-async function processCurrentResults(currentResults: SelectScoreWithRelations[], message: any) {
-  try {
-    if (currentResults.length > 0) {
-      const winners: SelectScoreWithRelations[] = await determineWinners(currentResults);
-      if (winners.length > 0) {
-        await informLatestResults(winners, message);
-      }
-    } else {
-      console.log('No results from processing the latest Wordle result.');
-    }
-  } catch (error) {
-    console.error('Error processing Wordle Result:', error);
-  }
-}
-
-async function determineWinners(results: SelectScoreWithRelations[]): Promise<SelectScoreWithRelations[]> {
-  if (!results || results.length === 0) return [];
-
-  // Filter out failed attempts (X) before processing
-  const validResults = results.filter(score => score.attempts.toUpperCase() !== 'X');
-
-  if (validResults.length === 0) return [];
-
-  // Convert attempts to numbers for comparison
-  const resultsWithNumericAttempts = validResults.map(result => ({
-    ...result,
-    numericAttempts: parseInt(result.attempts)
-  }));
-
-  // Find minimum attempts
-  const minAttempts = Math.min(
-    ...resultsWithNumericAttempts.map(result => result.numericAttempts)
-  );
-
-  // Return all scores that match minimum attempts
-  return validResults.filter((_, index) =>
-    resultsWithNumericAttempts[index].numericAttempts === minAttempts
-  );
-}
-
-async function informLatestResults(winners: SelectScoreWithRelations[], message: any) {
-  const winnerDiscordIds = winners.map(winner => winner.discordId);
-  const winnerDiscordTags = winnerDiscordIds.map(id => `<@${id}>`);
-
-  const gameNumber = winners[0].gameNumber || 1;
-  const winningAttempts = winners[0].attempts === 'X' ? 0 : parseInt(winners[0].attempts);
-  const winnerTags = winnerDiscordTags.join(', ');
-
-  const winnerMessage = `Current Winner${winners.length > 1 ? "s" : ""} for Wordle ${gameNumber.toLocaleString()} with ${winningAttempts} attempt${winningAttempts !== 0 && winningAttempts > 1 ? 's' : ''}: ${winnerTags}`;
-
-  console.log(winnerMessage);
-  await message.channel.send(winnerMessage);
-}
